3f45a3c1bb7daf448f742a8381f8e07c6c22ae50
Gilberto Masetto <gilberto.masetto@gmail.com>
2019-12-02 22:55:27 -0300

    Initial Commit

---
+++ mynoteslist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mynoteslist
+//
+//  Created by Gilberto Masetto on 02/12/19.
+//  Copyright © 2019 Gilberto Masetto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
